c92ba20dbc865cf41b72d307f179427a1857766e
mkujun <kujundzic.marko@hotmail.com>
2018-03-15 11:14:33 +0100

    changed domain model of release, backend adjustments

--- DatabaseScripts/07_ips_release.sql
+++ DatabaseScripts/07_ips_release.sql
@@ -1,8 +1,20 @@
 create table ips_release (
-  id int not null,
+  id number(10,0) not null,
+  release_version varchar2(3000 char) not null,
   release_date date,
   constraint rel_pk primary key (id)
   )
+
+create sequence ips_release_seq start with 1 increment by 1;
+
+  create or replace trigger ips_release_trig
+    before insert on ips_release
+    for each row
+    begin
+        select ips_release_seq.nextval
+        into :new.ID
+        from dual;
+    end;
 
 alter table ips_defects add release_id int;
 alter table ips_defects add constraint fk_release_id foreign key (release_id) references ips_release(id);
